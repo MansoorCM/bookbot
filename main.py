312528ac8aceb9036a89672a7d23d0e934446fca
MansoorCM <manzoorcm96@gmail.com>
2024-01-20 16:19:15 +0530

    print book report

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def main():
     path = "books/frankenstein.txt"
     text = get_file_text(path)
-    print(text)
-    print(f"word count is {get_word_count(text)}")
-    print(f"letter count is {get_letter_count(text)}")
+    word_count = get_word_count(text)
+    chars = get_letter_count(text)
+    print_report(chars, word_count, path)
 
 def get_file_text(path):
     with open(path) as f:
@@ -21,6 +21,20 @@ def get_letter_count(text):
         else:
             chars[lowered] += 1
     return chars
+
+def print_report(chars, word_count, path):
+    letters = []
+    for char in chars:
+        if char.isalpha():
+            letters.append([chars[char], char])
+    letters.sort(reverse = True)
+    print(f"--- Begin report of {path} ---")
+    print(f"{word_count} words found in the document")
+    print()
+    for count, char in letters:
+        print(f"The '{char}' character was found {count} times")
+    print("--- End report ---")
+
 
 if __name__ == '__main__':
     main()
